4f16c8c0a6e394fcd0f5ddd7b2eb914b0debbefa
baegteun <baegteun@gmail.com>
2023-03-20 14:24:11 +0900

    :memo: :: 모듈 경로 정상화

--- Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+Target.swift
+++ Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+Target.swift
@@ -15,7 +15,7 @@ public extension TargetDependency.Feature {
     )
     static let MainFeature = TargetDependency.project(
         target: ModulePaths.Feature.MainFeature.targetName(type: .sources),
-        path: .relativeToFeature(ModulePaths.Feature.MainFeature.targetName(type: .sources))
+        path: .relativeToFeature(ModulePaths.Feature.MainFeature.rawValue)
     )
     static let RootFeatureInterface = TargetDependency.project(
         target: ModulePaths.Feature.RootFeature.targetName(type: .interface),
@@ -23,7 +23,7 @@ public extension TargetDependency.Feature {
     )
     static let RootFeature = TargetDependency.project(
         target: ModulePaths.Feature.RootFeature.targetName(type: .sources),
-        path: .relativeToFeature(ModulePaths.Feature.RootFeature.targetName(type: .sources))
+        path: .relativeToFeature(ModulePaths.Feature.RootFeature.rawValue)
     )
     static let SigninFeatureInterface = TargetDependency.project(
         target: ModulePaths.Feature.SigninFeature.targetName(type: .interface),
@@ -31,51 +31,51 @@ public extension TargetDependency.Feature {
     )
     static let SigninFeature = TargetDependency.project(
         target: ModulePaths.Feature.SigninFeature.targetName(type: .sources),
-        path: .relativeToFeature(ModulePaths.Feature.SigninFeature.targetName(type: .sources))
+        path: .relativeToFeature(ModulePaths.Feature.SigninFeature.rawValue)
     )
     static let BaseFeature = TargetDependency.project(
         target: ModulePaths.Feature.BaseFeature.targetName(type: .sources),
-        path: .relativeToFeature(ModulePaths.Feature.BaseFeature.targetName(type: .sources))
+        path: .relativeToFeature(ModulePaths.Feature.BaseFeature.rawValue)
     )
 }
 
 public extension TargetDependency.Domain {
     static let AuthDomainTesting = TargetDependency.project(
         target: ModulePaths.Domain.AuthDomain.targetName(type: .testing),
-        path: .relativeToDomain(ModulePaths.Domain.AuthDomain.targetName(type: .testing))
+        path: .relativeToDomain(ModulePaths.Domain.AuthDomain.rawValue)
     )
     static let AuthDomainInterface = TargetDependency.project(
         target: ModulePaths.Domain.AuthDomain.targetName(type: .interface),
-        path: .relativeToDomain(ModulePaths.Domain.AuthDomain.targetName(type: .interface))
+        path: .relativeToDomain(ModulePaths.Domain.AuthDomain.rawValue)
     )
     static let AuthDomain = TargetDependency.project(
         target: ModulePaths.Domain.AuthDomain.targetName(type: .sources),
-        path: .relativeToDomain(ModulePaths.Domain.AuthDomain.targetName(type: .sources))
+        path: .relativeToDomain(ModulePaths.Domain.AuthDomain.rawValue)
     )
     static let BaseDomain = TargetDependency.project(
         target: ModulePaths.Domain.BaseDomain.targetName(type: .sources),
-        path: .relativeToDomain(ModulePaths.Domain.BaseDomain.targetName(type: .sources))
+        path: .relativeToDomain(ModulePaths.Domain.BaseDomain.rawValue)
     )
 }
 
 public extension TargetDependency.Core {
     static let DWebKit = TargetDependency.project(
         target: ModulePaths.Core.DWebKit.targetName(type: .sources),
-        path: .relativeToCore(ModulePaths.Core.DWebKit.targetName(type: .sources))
+        path: .relativeToCore(ModulePaths.Core.DWebKit.rawValue)
     )
     static let DesignSystem = TargetDependency.project(
         target: ModulePaths.Core.DesignSystem.targetName(type: .sources),
-        path: .relativeToCore(ModulePaths.Core.DesignSystem.targetName(type: .sources))
+        path: .relativeToCore(ModulePaths.Core.DesignSystem.rawValue)
     )
 }
 
 public extension TargetDependency.Shared {
     static let UtilityModule = TargetDependency.project(
         target: ModulePaths.Shared.UtilityModule.targetName(type: .sources),
-        path: .relativeToShared(ModulePaths.Shared.UtilityModule.targetName(type: .sources))
+        path: .relativeToShared(ModulePaths.Shared.UtilityModule.rawValue)
     )
     static let GlobalThirdPartyLibrary = TargetDependency.project(
         target: ModulePaths.Shared.GlobalThirdPartyLibrary.targetName(type: .sources),
-        path: .relativeToShared(ModulePaths.Shared.GlobalThirdPartyLibrary.targetName(type: .sources))
+        path: .relativeToShared(ModulePaths.Shared.GlobalThirdPartyLibrary.rawValue)
     )
 }
