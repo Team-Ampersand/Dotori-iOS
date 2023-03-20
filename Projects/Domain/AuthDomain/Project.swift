12ded0d0106c5edda174986f086af912f52a4b02
baegteun <baegteun@gmail.com>
2023-03-20 15:00:31 +0900

    :sparkles: :: Auth Dependency with JwtStore

--- Projects/Domain/AuthDomain/Project.swift
+++ Projects/Domain/AuthDomain/Project.swift
@@ -6,5 +6,7 @@ let project = Project.makeModule(
     name: ModulePaths.Domain.AuthDomain.rawValue,
     product: .staticLibrary,
     targets: [.interface, .testing, .unitTest],
-    internalDependencies: []
+    internalDependencies: [
+        .Core.JwtStoreInterface
+    ]
 )
