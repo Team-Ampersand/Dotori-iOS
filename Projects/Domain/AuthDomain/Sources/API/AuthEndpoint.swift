ea3dbbcff2132b0ebf9d450163fed0fcd65f7958
baegteun <baegteun@gmail.com>
2023-03-20 17:38:05 +0900

    :sparkles: :: AuthEndpoint

--- Projects/Domain/AuthDomain/Sources/API/AuthEndpoint.swift
+++ Projects/Domain/AuthDomain/Sources/API/AuthEndpoint.swift
@@ -1,6 +1,22 @@
+import BaseDomain
 import Emdpoint
 
 enum AuthEndpoint {
     case signin
 }
 
+extension AuthEndpoint: DotoriEndpoint {
+    typealias ErrorType = Error
+
+    var route: Route {
+        .post("")
+    }
+
+    var task: HTTPTask {
+        .requestPlain
+    }
+
+    var errorMapper: [Int : ErrorType]? {
+        [:]
+    }
+}
