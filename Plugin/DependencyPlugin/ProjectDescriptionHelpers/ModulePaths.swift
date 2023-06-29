d72c4eba9ff0615b0c6c2137eb3fe752b897b70e
baegteun <baegteun@gmail.com>
2023-06-29 11:25:41 +0900

    :sparkles: :: KeyValueStore Module

--- Plugin/DependencyPlugin/ProjectDescriptionHelpers/ModulePaths.swift
+++ Plugin/DependencyPlugin/ProjectDescriptionHelpers/ModulePaths.swift
@@ -48,6 +48,7 @@ public extension ModulePaths {
 
 public extension ModulePaths {
     enum Core: String, MicroTargetPathConvertable {
+        case KeyValueStore
         case JwtStore
     }
 }
---
+++ Projects/Core/KeyValueStore/Project.swift
@@ -0,0 +1,19 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+import DependencyPlugin
+
+let project = Project.module(
+    name: ModulePaths.Core.KeyValueStore.rawValue,
+    targets: [
+        .interface(module: .core(.KeyValueStore)),
+        .implements(module: .core(.KeyValueStore), dependencies: [
+            .core(target: .KeyValueStore, type: .interface)
+        ]),
+        .testing(module: .core(.KeyValueStore), dependencies: [
+            .core(target: .KeyValueStore, type: .interface)
+        ]),
+        .tests(module: .core(.KeyValueStore), dependencies: [
+            .core(target: .KeyValueStore)
+        ])
+    ]
+)
