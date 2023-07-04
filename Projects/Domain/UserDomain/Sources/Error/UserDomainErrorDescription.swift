5dafbd47a74a8fa6b3de9cdfc07c251a34faafea
baegteun <baegteun@gmail.com>
2023-07-04 15:10:08 +0900

    :art: :: 유저에게 보이지 않는 에러 메시지는 기본 영어로만 표시

--- Projects/Domain/UserDomain/Sources/Error/UserDomainErrorDescription.swift
+++ Projects/Domain/UserDomain/Sources/Error/UserDomainErrorDescription.swift
@@ -6,7 +6,7 @@ extension UserDomainError: LocalizedError {
     public var errorDescription: String? {
         switch self {
         case .cannotFindUserRole:
-            return "유저의 권한을 찾을 수 없습니다. 다시 로그인해주세요."
+            return "Cannot find user role"
         }
     }
 }
