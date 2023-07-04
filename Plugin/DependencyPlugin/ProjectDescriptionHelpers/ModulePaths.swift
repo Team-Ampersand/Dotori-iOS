4004194aaf3dcba93c2b296eee792dba50464195
baegteun <baegteun@gmail.com>
2023-07-04 12:09:02 +0900

    :sparkles: :: UserDomain Module

--- Plugin/DependencyPlugin/ProjectDescriptionHelpers/ModulePaths.swift
+++ Plugin/DependencyPlugin/ProjectDescriptionHelpers/ModulePaths.swift
@@ -41,6 +41,7 @@ public extension ModulePaths {
 
 public extension ModulePaths {
     enum Domain: String, MicroTargetPathConvertable {
+        case UserDomain
         case AuthDomain
         case BaseDomain
     }
