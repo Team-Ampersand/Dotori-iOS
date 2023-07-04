96f8839a5a690b94e0bb9daf848cc4f9d0f3b3b5
baegteun <baegteun@gmail.com>
2023-07-04 13:43:30 +0900

    :sparkles: :: 유저 권한 저장 로직 추가

--- Projects/Domain/BaseDomain/Sources/Base/BaseRemoteDataSource.swift
+++ Projects/Domain/BaseDomain/Sources/Base/BaseRemoteDataSource.swift
@@ -3,30 +3,42 @@ import DateUtility
 import Emdpoint
 import Foundation
 import JwtStoreInterface
+import KeyValueStoreInterface
 
 open class BaseRemoteDataSource<Endpoint: DotoriEndpoint>:
     HasJwtStore,
+    HasKeyValueStore,
     HasEmdpointClient,
     HasJSONDecoder,
     RemoteRequestable {
     public let jwtStore: any JwtStore
+    public let keyValueStore: any KeyValueStore
     public let client: EmdpointClient<Endpoint>
     public let decoder: JSONDecoder
     public let maxRetryCount = 2
 
     public init(
         jwtStore: any JwtStore,
+        keyValueStore: any KeyValueStore,
         client: EmdpointClient<Endpoint>? = nil,
         decoder: JSONDecoder = JSONDecoder()
     ) {
         self.jwtStore = jwtStore
+        self.keyValueStore = keyValueStore
         #if DEV || STAGE
         self.client = EmdpointClient(
-            interceptors: [JwtInterceptor(jwtStore: jwtStore), DotoriLoggingInterceptor()]
+            interceptors: [
+                JwtInterceptor(jwtStore: jwtStore),
+                DotoriLoggingInterceptor(),
+                DotoriRoleInterceptor(keyValueStore: keyValueStore)
+            ]
         )
         #else
         self.client = EmdpointClient(
-            interceptors: [JwtInterceptor(jwtStore: jwtStore)]
+            interceptors: [
+                JwtInterceptor(jwtStore: jwtStore),
+                DotoriRoleInterceptor(keyValueStore: keyValueStore)
+            ]
         )
         #endif
         self.decoder = decoder
---
+++ Projects/Domain/BaseDomain/Sources/Interceptor/Role/DotoriRoleDTO.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct DotoriRoleDTO: Decodable {
+    let roles: [String]
+}
