799731b65e751d1c69edc10a2b30e14dd0d8519b
baegteun <baegteun@gmail.com>
2023-07-21 14:36:50 +0900

    :lipstick: :: [#79] NoticeFeature / NoticeCell UI

--- Projects/Feature/NoticeFeature/Sources/Scene/NoticeViewController.swift
+++ Projects/Feature/NoticeFeature/Sources/Scene/NoticeViewController.swift
@@ -3,6 +3,7 @@ import DesignSystem
 import Localization
 import MSGLayout
 import UIKit
+import UIKitUtil
 
 final class NoticeViewController: BaseViewController<NoticeStore> {
     private enum Metric {
@@ -11,10 +12,17 @@ final class NoticeViewController: BaseViewController<NoticeStore> {
     }
     private let dotoriBarButton = DotoriBarButtonItem()
     private let entireTitleLabel = DotoriLabel(L10n.Notice.entireTitle, font: .subtitle1)
+    private let noticeTableView = UITableView()
+        .set(\.backgroundColor, .clear)
+        .set(\.separatorStyle, .none)
+        .then {
+            $0.register(cellType: NoticeCell.self)
+        }
 
     override func addView() {
         view.addSubviews {
             entireTitleLabel
+            noticeTableView
         }
     }
 
@@ -23,10 +31,30 @@ final class NoticeViewController: BaseViewController<NoticeStore> {
             entireTitleLabel.layout
                 .top(.to(view.safeAreaLayoutGuide), .equal(13))
                 .leading(.toSuperview(), .equal(Metric.horizontalPadding))
+
+            noticeTableView.layout
+                .top(.to(entireTitleLabel).bottom, .equal(24))
+                .horizontal(.toSuperview(), .equal(Metric.horizontalPadding))
+                .bottom(.toSuperview())
         }
+        noticeTableView.dataSource = self
     }
 
     override func configureNavigation() {
         self.navigationItem.setLeftBarButton(dotoriBarButton, animated: true)
+    }
+}
+
+extension NoticeViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(for: indexPath, cellType: NoticeCell.self)
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
     }
 }
--- Projects/Feature/NoticeFeature/Sources/Scene/View/NoticeCell.swift
+++ Projects/Feature/NoticeFeature/Sources/Scene/View/NoticeCell.swift
@@ -1,6 +1,48 @@
 import BaseFeature
+import DesignSystem
+import MSGLayout
 import UIKit
 
 final class NoticeCell: BaseTableViewCell<Void> {
-    
+    private let signatureView = UIView()
+        .set(\.backgroundColor, .blue)
+        .set(\.cornerRadius, 6)
+        .set(\.clipsToBounds, true)
+    private let authorLabel = DotoriLabel("도토리", font: .smalltitle)
+    private let dateLabel = DotoriLabel("2023.07.21", textColor: .neutral(.n20), font: .caption)
+    private let titleLabel = DotoriLabel("[자습실 관련 공지]", textColor: .neutral(.n20), font: .caption)
+    private let contentLabel = DotoriLabel("[자습실 관련 공지]", textColor: .neutral(.n20), font: .caption)
+        .set(\.numberOfLines, 2)
+
+    override func setLayout() {
+        MSGLayout.stackedLayout(self.contentView) {
+            VStackView(spacing: 8) {
+                HStackView(spacing: 4) {
+                    signatureView
+                        .width(12)
+                        .height(12)
+
+                    authorLabel
+
+                    SpacerView()
+
+                    dateLabel
+                }
+
+                VStackView(spacing: 2) {
+                    titleLabel
+
+                    contentLabel
+                }
+            }
+            .margin(.init(top: 12, left: 16, bottom: 12, right: 16))
+        }
+    }
+
+    override func configureView() {
+        self.backgroundColor = .clear
+        self.backgroundColor = .dotori(.neutral(.n50))
+        self.cornerRadius = 8
+        self.clipsToBounds = true
+    }
 }
