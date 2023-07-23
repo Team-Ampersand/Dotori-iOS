a48c5238a6740eed41f6da5593fd234daafac2c5
baegteun <baegteun@gmail.com>
2023-07-23 15:09:59 +0900

    :white_check_mark: :: [#79] NoticeFeature / 공지 가져오는 상태와 Section으로 분리된 공지 상태 Tests

--- Projects/Feature/NoticeFeature/Tests/NoticeFeatureTest.swift
+++ Projects/Feature/NoticeFeature/Tests/NoticeFeatureTest.swift
@@ -1,4 +1,6 @@
+import BaseDomainInterface
 import Combine
+import NoticeDomainInterface
 import XCTest
 @testable import NoticeFeature
 @testable import NoticeDomainTesting
@@ -27,7 +29,61 @@ final class NoticeFeatureTests: XCTestCase {
         subscription = nil
     }
 
-    func testViewDidLoad() {
-        XCTAssertEqual(1, 1)
+    func test_LoadCurrentUserRole_When_ViewDidLoad() {
+        let expectation = XCTestExpectation(description: "viewDidLoad expectation")
+        let expected = UserRoleType.developer
+        loadCurrentUserRoleUseCase.loadCurrentUserRoleReturn = expected
+
+        sut.state.map(\.currentUserRole).sink { _ in
+            expectation.fulfill()
+        }
+        .store(in: &subscription)
+
+        XCTAssertEqual(loadCurrentUserRoleUseCase.loadCurrentUserRoleCallCount, 0)
+        sut.send(.viewDidLoad)
+
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertEqual(expected, sut.currentState.currentUserRole)
+        XCTAssertEqual(loadCurrentUserRoleUseCase.loadCurrentUserRoleCallCount, 1)
+    }
+
+    func test_FetchNoticeList_When_ViewWillAppear() {
+        let expectation = XCTestExpectation(description: "viewWillAppear expectation")
+        expectation.expectedFulfillmentCount = 2
+
+        let todayMonth = DateComponents(
+            calendar: .init(identifier: .iso8601),
+            year: 2023,
+            month: 7,
+            day: 23
+        ).date!
+        let nextMonth = todayMonth.addingTimeInterval(86400 * 31)
+        let expected = [
+            NoticeModel(id: 1, title: "title", content: "content", roles: .councillor, createdTime: todayMonth),
+            NoticeModel(id: 2, title: "title2", content: "conten2t", roles: .developer, createdTime: nextMonth)
+        ]
+        let sectiondExpected: [SectiondNoticeTuple] = [
+            ("2023년 07월", [expected[0]]),
+            ("2023년 08월", [expected[1]])
+        ]
+        fetchNoticeListUseCase.fetchNoticeListReturn = expected
+
+        sut.state.map(\.noticeList).sink { _ in
+            expectation.fulfill()
+        }
+        .store(in: &subscription)
+
+        XCTAssertEqual(fetchNoticeListUseCase.fetchNoticeListCallCount, 0)
+        sut.send(.viewWillAppear)
+
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertEqual(expected, sut.currentState.noticeList)
+
+        XCTAssertEqual(sectiondExpected[0].section, sut.currentState.noticeSectionList[0].section)
+        XCTAssertEqual(sectiondExpected[0].noticeList, sut.currentState.noticeSectionList[0].noticeList)
+        XCTAssertEqual(sectiondExpected[1].section, sut.currentState.noticeSectionList[1].section)
+        XCTAssertEqual(sectiondExpected[1].noticeList, sut.currentState.noticeSectionList[1].noticeList)
+
+        XCTAssertEqual(fetchNoticeListUseCase.fetchNoticeListCallCount, 1)
     }
 }
