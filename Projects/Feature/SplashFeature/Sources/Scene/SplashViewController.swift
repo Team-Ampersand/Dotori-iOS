5991ba5b901f3939dcd9fd8eb07e8a10d91e2472
baegteun <baegteun@gmail.com>
2023-07-23 18:21:17 +0900

    :lipstick: :: [#72] App / SplashFeature BackgroundColor 및 아이콘 변경

--- Projects/Feature/SplashFeature/Sources/Scene/SplashViewController.swift
+++ Projects/Feature/SplashFeature/Sources/Scene/SplashViewController.swift
@@ -1,5 +1,20 @@
 import BaseFeature
+import DesignSystem
+import MSGLayout
 import UIKit
 
 final class SplashViewController: BaseViewController<SplashStore> {
+    private let dotoriLogoImageView = DotoriIconView(image: .Dotori.dotori)
+
+    override func addView() {
+        view.addSubviews {
+            dotoriLogoImageView
+        }
+    }
+
+    override func setLayout() {
+        MSGLayout.buildLayout {
+            dotoriLogoImageView
+        }
+    }
 }
