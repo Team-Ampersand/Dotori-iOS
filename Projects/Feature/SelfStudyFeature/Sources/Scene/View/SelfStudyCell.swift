dd8e6c18b9357339f165b85f03ec2c49526b5929
baegteun <baegteun@gmail.com>
2023-07-25 00:16:17 +0900

    :sparkles: :: [#96] SelfStudyFeature / SelfStudyCell model adapt

--- Projects/Feature/SelfStudyFeature/Sources/Scene/View/SelfStudyCell.swift
+++ Projects/Feature/SelfStudyFeature/Sources/Scene/View/SelfStudyCell.swift
@@ -1,22 +1,23 @@
 import BaseFeature
 import DesignSystem
 import MSGLayout
+import SelfStudyDomainInterface
 import UIKit
 
-final class SelfStudyCell: BaseTableViewCell<Void> {
+final class SelfStudyCell: BaseTableViewCell<SelfStudyRankModel> {
     private let containerView = UIView()
-    private let rankLabel = DotoriLabel("1", textColor: .neutral(.n20), font: .caption)
+    private let rankLabel = DotoriLabel(textColor: .neutral(.n20), font: .caption)
     private let selfStudyCheckBox = DotoriCheckBox()
     private let userImageView = DotoriIconView(
         size: .custom(.init(width: 64, height: 64)),
         image: .Dotori.personRectangle
     )
-    private let nameLabel = DotoriLabel("김준", textColor: .neutral(.n10), font: .body1)
+    private let nameLabel = DotoriLabel(textColor: .neutral(.n10), font: .body1)
     private let genderImageView = DotoriIconView(
         size: .custom(.init(width: 16, height: 16)),
         image: .Dotori.men
     )
-    private let gradeClassLabel = DotoriLabel("3218", textColor: .neutral(.n20), font: .caption)
+    private let stuNumLabel = DotoriLabel(textColor: .neutral(.n20), font: .caption)
     private lazy var profileStackView = VStackView(spacing: 8) {
         userImageView
 
@@ -27,7 +28,7 @@ final class SelfStudyCell: BaseTableViewCell<Void> {
         }
         .alignment(.center)
 
-        gradeClassLabel
+        stuNumLabel
     }.alignment(.center)
 
     override func addView() {
@@ -68,5 +69,12 @@ final class SelfStudyCell: BaseTableViewCell<Void> {
         self.containerView.cornerRadius = 16
         self.backgroundColor = .clear
         self.selectionStyle = .none
+    }
+
+    override func adapt(model: SelfStudyRankModel) {
+        self.rankLabel.text = "\(model.rank)"
+        self.nameLabel.text = model.memberName
+        self.genderImageView.image = model.gender == .man ? .Dotori.men : .Dotori.women
+        self.stuNumLabel.text = model.stuNum
     }
 }
