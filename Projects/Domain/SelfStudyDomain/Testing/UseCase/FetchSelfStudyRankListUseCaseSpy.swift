ca7d17e68684d6fd3db77d9032f5789ec1a6b58c
baegteun <baegteun@gmail.com>
2023-07-25 00:10:22 +0900

    :white_check_mark: :: [#96] SelfStudyDomain / FetchSelfStudyRankListUseCaseTests

--- Projects/Domain/SelfStudyDomain/Testing/UseCase/FetchSelfStudyRankListUseCaseSpy.swift
+++ Projects/Domain/SelfStudyDomain/Testing/UseCase/FetchSelfStudyRankListUseCaseSpy.swift
@@ -3,7 +3,7 @@ import SelfStudyDomainInterface
 final class FetchSelfStudyRankListUseCaseSpy: FetchSelfStudyRankListUseCase {
     var fetchSelfStudyRankListCallCount = 0
     var fetchSelfStudyRankListReturn: [SelfStudyRankModel] = []
-    func callAsFunction() async throws -> [SelfStudyRankModel] {
+    func callAsFunction(req: FetchSelfStudyRankSearchRequestDTO?) async throws -> [SelfStudyRankModel] {
         fetchSelfStudyRankListCallCount += 1
         return fetchSelfStudyRankListReturn
     }
