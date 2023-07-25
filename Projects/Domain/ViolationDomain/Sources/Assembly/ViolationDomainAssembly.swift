9c03f563dfc5dc82f74f42fa90a53330ace15396
baegteun <baegteun@gmail.com>
2023-07-25 23:04:17 +0900

    :truck: :: [#94] ViolationDomain / ViolationAssembly -> ViolationDomainAssembly

--- Projects/Domain/ViolationDomain/Sources/Assembly/ViolationDomainAssembly.swift
+++ Projects/Domain/ViolationDomain/Sources/Assembly/ViolationDomainAssembly.swift
@@ -2,14 +2,14 @@ import NetworkingInterface
 import Swinject
 import ViolationDomainInterface
 
-public final class ViolationAssembly: Assembly {
+public final class ViolationDomainAssembly: Assembly {
     public init() {}
     public func assemble(container: Container) {
         container.register(RemoteViolationDataSource.self) { resolver in
             RemoteViolationDataSourceImpl(networking: resolver.resolve(Networking.self)!)
         }
         .inObjectScope(.container)
-    
+
         container.register(ViolationRepository.self) { resolver in
             ViolationRepositoryImpl(remoteViolationDataSource: resolver.resolve(RemoteViolationDataSource.self)!)
         }
