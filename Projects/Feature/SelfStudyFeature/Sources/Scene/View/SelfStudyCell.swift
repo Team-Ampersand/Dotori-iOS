bdf171c514fc5acb50f42beea3cf6c8819b58d51
baegteun <baegteun@gmail.com>
2023-07-26 12:07:07 +0900

    :recycle: :: [#101] DesignSystem / 신청한 학생의 CardView 재사용성 주기

--- Projects/Feature/SelfStudyFeature/Sources/Scene/View/SelfStudyCell.swift
+++ Projects/Feature/SelfStudyFeature/Sources/Scene/View/SelfStudyCell.swift
@@ -12,32 +12,7 @@ protocol SelfStudyCellDelegate: AnyObject {
 
 final class SelfStudyCell: BaseTableViewCell<SelfStudyRankModel> {
     weak var delegate: (any SelfStudyCellDelegate)?
-    private let containerView = UIView()
-    private let rankLabel = DotoriLabel(textColor: .neutral(.n20), font: .caption)
-    private let selfStudyCheckBox = DotoriCheckBox()
-        .set(\.isHidden, true)
-    private let userImageView = DotoriIconView(
-        size: .custom(.init(width: 64, height: 64)),
-        image: .Dotori.personRectangle
-    )
-    private let nameLabel = DotoriLabel(textColor: .neutral(.n10), font: .body1)
-    private let genderImageView = DotoriIconView(
-        size: .custom(.init(width: 16, height: 16)),
-        image: .Dotori.men
-    )
-    private let stuNumLabel = DotoriLabel(textColor: .neutral(.n20), font: .caption)
-    private lazy var profileStackView = VStackView(spacing: 8) {
-        userImageView
-
-        HStackView(spacing: 2) {
-            nameLabel
-
-            genderImageView
-        }
-        .alignment(.center)
-
-        stuNumLabel
-    }.alignment(.center)
+    private let appliedStudentCardView = AppliedStudentCardView()
     private let medalImageView = DotoriIconView(
         size: .custom(.init(width: 56, height: 80)),
         image: nil
@@ -60,35 +35,18 @@ final class SelfStudyCell: BaseTableViewCell<SelfStudyRankModel> {
 
     override func addView() {
         contentView.addSubviews {
-            containerView
+            appliedStudentCardView
         }
-        containerView.addSubviews {
-            rankLabel
-            selfStudyCheckBox
-            profileStackView
+        appliedStudentCardView.addSubviews {
             medalImageView
         }
     }
 
     override func setLayout() {
         MSGLayout.buildLayout {
-            containerView.layout
+            appliedStudentCardView.layout
                 .horizontal(.toSuperview(), .equal(20))
                 .vertical(.toSuperview(), .equal(12))
-
-            rankLabel.layout
-                .top(.toSuperview(), .equal(12))
-                .leading(.toSuperview(), .equal(16))
-
-            selfStudyCheckBox.layout
-                .top(.toSuperview(), .equal(12))
-                .trailing(.toSuperview(), .equal(-12))
-                .size(24)
-
-            profileStackView.layout
-                .centerX(.toSuperview())
-                .top(.toSuperview(), .equal(28))
-                .bottom(.toSuperview(), .equal(-28))
 
             medalImageView.layout
                 .trailing(.toSuperview(), .equal(-8))
@@ -97,31 +55,32 @@ final class SelfStudyCell: BaseTableViewCell<SelfStudyRankModel> {
     }
 
     override func configureView() {
-        self.containerView.backgroundColor = .dotori(.background(.card))
-        self.containerView.cornerRadius = 16
-        DotoriShadow.cardShadow(card: self.containerView)
         self.backgroundColor = .clear
         self.selectionStyle = .none
     }
 
     override func adapt(model: SelfStudyRankModel) {
         super.adapt(model: model)
-        self.rankLabel.text = "\(model.rank)"
-        self.nameLabel.text = model.memberName
-        self.genderImageView.image = model.gender == .man ? .Dotori.men : .Dotori.women
-        self.stuNumLabel.text = model.stuNum
-        self.selfStudyCheckBox.isChecked = model.selfStudyCheck
+        self.appliedStudentCardView.updateContent(
+            with: .init(
+                rank: model.rank,
+                name: model.memberName,
+                gender: model.gender == .man ? .man : .woman,
+                stuNum: model.stuNum,
+                isChecked: model.selfStudyCheck
+            )
+        )
         self.medalImageView.image = self.rankToImage(rank: model.rank)
     }
 
     func setUserRole(userRole: UserRoleType) {
-        selfStudyCheckBox.isHidden = userRole == .member
+        appliedStudentCardView.setIsHiddenAttendanceCheckBox(userRole == .member)
     }
 }
 
 private extension SelfStudyCell {
     func bindAction() {
-        selfStudyCheckBox.checkBoxDidTapPublisher
+        appliedStudentCardView.attendanceCheckBoxPublisher
             .throttle(for: 1, scheduler: RunLoop.main, latest: true)
             .compactMap { [weak self] (checked) -> (Int, Bool)? in
                 guard let id = self?.model?.id else { return nil }
