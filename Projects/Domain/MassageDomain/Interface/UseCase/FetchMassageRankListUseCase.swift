7db3ef12e4f982bfb0579a6b0797cac20dc923ab
baegteun <baegteun@gmail.com>
2023-07-26 13:20:08 +0900

    :truck: :: [#101] BaseDomainInterface / SelfStudyDomainInterface GenderType -> BaseDomainInterface GenderType

---
+++ Projects/Domain/MassageDomain/Interface/UseCase/FetchMassageRankListUseCase.swift
@@ -0,0 +1,9 @@
+//
+//  FetchMassageRankListUseCase.swift
+//  MassageDomainInterface
+//
+//  Created by 최형우 on 2023/07/26.
+//  Copyright © 2023 com.msg. All rights reserved.
+//
+
+import Foundation
