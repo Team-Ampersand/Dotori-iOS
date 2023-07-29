3c10e5212d4b2b195cb61248214d7ff6bc7dc046
baegteun <baegteun@gmail.com>
2023-07-29 22:37:49 +0900

    :white_check_mark: :: [#122] DetailNoticeFeature / removeNoticeUseCase DI

--- Projects/Feature/DetailNoticeFeature/Tests/DetailNoticeFeatureTest.swift
+++ Projects/Feature/DetailNoticeFeature/Tests/DetailNoticeFeatureTest.swift
@@ -5,21 +5,25 @@ import XCTest
 
 final class DetailNoticeFeatureTests: XCTestCase {
     var fetchNoticeUseCase: FetchNoticeUseCaseSpy!
+    var removeNoticeUseCase: RemoveNoticeUseCaseSpy!
     var loadCurrentUserRoleUseCase: LoadCurrentUserRoleUseCaseSpy!
     var sut: DetailNoticeStore!
 
     override func setUp() {
         fetchNoticeUseCase = .init()
+        removeNoticeUseCase = .init()
         loadCurrentUserRoleUseCase = .init()
         sut = .init(
             noticeID: 1,
             fetchNoticeUseCase: fetchNoticeUseCase,
-            loadCurrentUserRole: loadCurrentUserRoleUseCase
+            removeNoticeUseCase: removeNoticeUseCase,
+            loadCurrentUserRoleUseCase: loadCurrentUserRoleUseCase
         )
     }
 
     override func tearDown() {
         fetchNoticeUseCase = nil
+        removeNoticeUseCase = nil
         loadCurrentUserRoleUseCase = nil
         sut = nil
     }
