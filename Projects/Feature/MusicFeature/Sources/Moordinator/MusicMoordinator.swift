2f3eda95ddbb32fcbf7b0e2383a57e3baefceac9
baegteun <baegteun@gmail.com>
2023-08-01 20:08:42 +0900

    :ambulance: :: [#140] Moordinators / Alert를 띄울때 ActionSheet 스타일은 iPad일때 alert로 띄우도록 변경

--- Projects/Feature/MusicFeature/Sources/Moordinator/MusicMoordinator.swift
+++ Projects/Feature/MusicFeature/Sources/Moordinator/MusicMoordinator.swift
@@ -65,6 +65,7 @@ private extension MusicMoordinator {
         style: UIAlertController.Style,
         actions: [UIAlertAction]
     ) -> MoordinatorContributors {
+        let style: UIAlertController.Style = UIDevice.current.userInterfaceIdiom == .pad ? .alert : .actionSheet
         let alert = UIAlertController(title: title, message: message, preferredStyle: style)
         if !actions.isEmpty {
             actions.forEach(alert.addAction(_:))
