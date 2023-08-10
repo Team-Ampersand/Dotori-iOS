72ddbc31f5acf0c821f6bb49bbbe2f4524ebb577
baegteun <baegteun@gmail.com>
2023-08-10 16:19:19 +0900

    :bug: :: [#31] Script / BasedOnDependency false
    g

--- Tuist/ProjectDescriptionHelpers/Action+Template.swift
+++ Tuist/ProjectDescriptionHelpers/Action+Template.swift
@@ -3,11 +3,13 @@ import ProjectDescription
 public extension TargetScript {
     static let swiftLint = TargetScript.pre(
         path: Path.relativeToRoot("Scripts/SwiftLintRunScript.sh"),
-        name: "SwiftLint"
+        name: "SwiftLint",
+        basedOnDependencyAnalysis: false
     )
 
     static let swiftFormat = TargetScript.pre(
         path: Path.relativeToRoot("Scripts/SwiftFormatRunScript.sh"),
-        name: "SwiftFormat"
+        name: "SwiftFormat",
+        basedOnDependencyAnalysis: false
     )
 }
