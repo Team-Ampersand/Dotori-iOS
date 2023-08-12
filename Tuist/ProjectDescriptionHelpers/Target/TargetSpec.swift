a67e7ea1cad1a9b13db38170a6965f43fd1d8934
baegteun <baegteun@gmail.com>
2023-08-12 13:56:55 +0900

    :green_heart: :: [#159] App / CodeSign 최상위 모듈에만 적용

--- Tuist/ProjectDescriptionHelpers/Target/TargetSpec.swift
+++ Tuist/ProjectDescriptionHelpers/Target/TargetSpec.swift
@@ -90,7 +90,6 @@ public struct TargetSpec: Configurable {
             dependencies: dependencies,
             settings: settings ?? .settings(
                 base: env.baseSetting
-                    .merging(.codeSign)
                     .merging((product ?? self.product) == .framework ? .allLoadLDFlages : .ldFlages),
                 configurations: .default,
                 defaultSettings: .recommended
