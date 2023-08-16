e7c1deee435cf9015a3c0abd2b55d4c8b8f405fc
baegteun <baegteun@gmail.com>
2023-08-16 14:11:45 +0900

    :green_heart: :: [#162] AppStoreCD / mobileProvision 로컬에 등록

--- .github/workflows/AppStore/Keychain.sh
+++ .github/workflows/AppStore/Keychain.sh
@@ -8,3 +8,12 @@ security unlock-keychain -p "" "$KEYCHAIN"
 tuist signing decrypt
 security import "Tuist/Signing/dotori.p12" -k "$KEYCHAIN" -P "$CERTS_EXPORT_PWD" -A
 security set-key-partition-list -S apple-tool:,apple: -s -k "" "$KEYCHAIN"
+
+mkdir -p "$HOME/Library/MobileDevice/Provisioning Profiles" 
+cd "Tuist/Signing"
+echo `ls *.mobileprovision`
+for PROVISION in `ls *.mobileprovision`
+do
+  UUID=`/usr/libexec/PlistBuddy -c 'Print :UUID' /dev/stdin <<< $(security cms -D -i ./$PROVISION)`
+  cp "./$PROVISION" "$HOME/Library/MobileDevice/Provisioning Profiles/$UUID.mobileprovision"
+done
