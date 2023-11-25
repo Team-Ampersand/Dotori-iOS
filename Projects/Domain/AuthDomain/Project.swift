4cd84b8b3c08cf81da07dda313f4b6dfecdb4a9a
uuuunseo <jdbstjoo@gmail.com>
2023-11-26 00:18:21 +0900

    :heavy_plus_sign: :: [#210] GAuthSignin

--- Projects/Domain/AuthDomain/Project.swift
+++ Projects/Domain/AuthDomain/Project.swift
@@ -6,11 +6,13 @@ let project = Project.module(
     name: ModulePaths.Domain.AuthDomain.rawValue,
     targets: [
         .interface(module: .domain(.AuthDomain), dependencies: [
-            .domain(target: .BaseDomain, type: .interface)
+            .domain(target: .BaseDomain, type: .interface),
+            .SPM.GAuthSignin
         ]),
         .implements(module: .domain(.AuthDomain), dependencies: [
             .domain(target: .AuthDomain, type: .interface),
-            .domain(target: .BaseDomain)
+            .domain(target: .BaseDomain),
+            .SPM.GAuthSignin
         ]),
         .testing(module: .domain(.AuthDomain), dependencies: [
             .domain(target: .AuthDomain, type: .interface)
--- Projects/Feature/SigninFeature/Project.swift
+++ Projects/Feature/SigninFeature/Project.swift
@@ -6,14 +6,16 @@ let project = Project.module(
     name: ModulePaths.Feature.SigninFeature.rawValue,
     targets: [
         .interface(module: .feature(.SigninFeature), dependencies: [
-            .feature(target: .BaseFeature, type: .interface)
+            .feature(target: .BaseFeature, type: .interface),
+            .SPM.GAuthSignin
         ]),
         .implements(module: .feature(.SigninFeature), dependencies: [
             .feature(target: .BaseFeature),
             .feature(target: .SigninFeature, type: .interface),
             .feature(target: .SignupFeature, type: .interface),
             .feature(target: .RenewalPasswordFeature, type: .interface),
-            .domain(target: .AuthDomain, type: .interface)
+            .domain(target: .AuthDomain, type: .interface),
+            .SPM.GAuthSignin
         ]),
         .tests(module: .feature(.SigninFeature), dependencies: [
             .feature(target: .SigninFeature)
