d8d0d3f54c67840e4c858c563076d9043b1e0b52
uuuunseo <jdbstjoo@gmail.com>
2023-11-27 08:26:30 +0900

    :sparkles: :: [#210] SigninViewController / dotoriSubTitle 추가, 레이아웃 수정

--- Projects/Feature/SigninFeature/Sources/Scene/SigninViewController.swift
+++ Projects/Feature/SigninFeature/Sources/Scene/SigninViewController.swift
@@ -13,47 +13,57 @@ final class SigninViewController: BaseStoredViewController<SigninStore> {
     private let dotoriLogoImageView = UIImageView()
         .set(
             \.image,
-            .Dotori.dotoriSigninLogo
+             .Dotori.dotoriSigninLogo
                 .withRenderingMode(.alwaysTemplate)
                 .withTintColor(.dotori(.primary(.p10)))
                 .resize(width: 182, height: 41)
         )
+    private let dotoriSubTitle = DotoriLabel(
+        "광주소프트웨어마이스터고\n기숙사 관리 시스템, DOTORI",
+        textColor: .neutral(.n20), font: .subtitle2)
+        .set(\.numberOfLines, 0)
+        .set(\.textAlignment, .center)
     private let signinButton = GAuthButton(
         auth: .signin,
         color: .colored,
         rounded: .default
     )
-
+    
     override func addView() {
         view.addSubviews {
             dotoriLogoImageView
+            dotoriSubTitle
             signinButton
         }
     }
-
+    
     override func setLayout() {
         MSGLayout.buildLayout {
             dotoriLogoImageView.layout
                 .centerX(.toSuperview())
-                .top(.to(view.safeAreaLayoutGuide).top, .equal(20))
+                .top(.to(view.safeAreaLayoutGuide).top, .equal(233))
                 .height(41)
-
+            
+            dotoriSubTitle.layout
+                .centerX(.toSuperview())
+                .top(.to(dotoriLogoImageView).bottom, .equal(21))
+            
             signinButton.layout
                 .centerX(.toSuperview())
                 .horizontal(.toSuperview(), .equal(20))
-                .bottom(.toSuperview(), .equal(-60))
+                .bottom(.to(view.safeAreaLayoutGuide), .equal(-60))
                 .height(50)
         }
     }
-
+    
     override func configureViewController() {
         self.view.backgroundColor = .dotori(.background(.card))
     }
-
+    
     override func configureNavigation() {
         self.navigationItem.title = L10n.Signin.loginNavigationTitle
     }
-
+    
     override func bindAction() {
         signinButton.tapPublisher
             .map { Store.Action.signinButtonDidTap }
@@ -61,3 +71,4 @@ final class SigninViewController: BaseStoredViewController<SigninStore> {
             .store(in: &subscription)
     }
 }
+
