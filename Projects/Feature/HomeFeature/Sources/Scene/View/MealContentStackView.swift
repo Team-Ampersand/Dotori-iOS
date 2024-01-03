45fc15d6ebc3be7ec95695b05c5d87d4a5736314
junseopark0331 <junseopark0331@icloud.com>
2024-01-03 14:21:17 +0900

    :recycle: ::[#184] HomeFeature / 공백제거

--- Projects/Feature/HomeFeature/Sources/Scene/View/MealContentStackView.swift
+++ Projects/Feature/HomeFeature/Sources/Scene/View/MealContentStackView.swift
@@ -12,7 +12,6 @@ final class MealContentStackView: UIStackView {
         self.isLayoutMarginsRelativeArrangement = true
         self.alignment = .leading
     }
-    
     @available(*, unavailable)
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -20,7 +19,6 @@ final class MealContentStackView: UIStackView {
     public func updateContent(meals: [String]) {
         self.removeAllChildren()
         var mealViews: [DotoriLabel] = []
-        
         if meals.isEmpty {
             let emptyLabels = (1...6).map { _ in DotoriLabel("") }
             mealViews.append(DotoriLabel("급식이 없습니다."))
@@ -28,7 +26,6 @@ final class MealContentStackView: UIStackView {
         } else {
             mealViews = meals.map { DotoriLabel($0) }
         }
-        
         for (index, view) in mealViews.enumerated() {
             UIView.animate(withDuration: 0.5, delay: 0.05 * Double(index), options: [], animations: {
                 view.alpha = 1.0
