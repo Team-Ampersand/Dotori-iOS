b3e601852882eb1075b9ef6d694560be5c314d1e
junseopark0331 <junseopark0331@icloud.com>
2024-03-01 23:27:57 +0900

    ✨ :: [#220] dashedBorder 사용을 위해 추가

--- Projects/UserInterface/DesignSystem/Sources/Extension/UIView+dashedLine.swift
+++ Projects/UserInterface/DesignSystem/Sources/Extension/UIView+dashedLine.swift
@@ -1,9 +1,22 @@
-//
-//  UIView+dashedLine.swift
-//  DesignSystem
-//
-//  Created by 박준서 on 3/1/24.
-//  Copyright © 2024 com.msg. All rights reserved.
-//
+import UIKit
 
-import Foundation
+public extension UIView {
+    func addDashedBorder() {
+        let color: UIColor = .dotori(.primary(.p10))
+
+        let shapeLayer:CAShapeLayer = CAShapeLayer()
+        let frameSize = self.frame.size
+        let shapeRect = CGRect(x: 0, y: 0, width: frameSize.width, height: frameSize.height)
+
+        shapeLayer.bounds = shapeRect
+        shapeLayer.position = CGPoint(x: frameSize.width/2, y: frameSize.height/2)
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = color.cgColor
+        shapeLayer.lineWidth = 2
+        shapeLayer.lineJoin = CAShapeLayerLineJoin.round
+        shapeLayer.lineDashPattern = [6,3]
+        shapeLayer.path = UIBezierPath(roundedRect: shapeRect, cornerRadius: 4).cgPath
+
+        self.layer.addSublayer(shapeLayer)
+    }
+}
