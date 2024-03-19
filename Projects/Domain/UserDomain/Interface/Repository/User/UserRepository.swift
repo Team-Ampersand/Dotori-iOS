379ff63e10274e770c1053f6b0e5d2651784c24e
junseopark0331 <junseopark0331@icloud.com>
2024-03-19 20:14:19 +0900

    ♻️ :: [#220] UserDomain / edit 삭제 및 fetch 추가

--- Projects/Domain/UserDomain/Interface/Repository/User/UserRepository.swift
+++ Projects/Domain/UserDomain/Interface/Repository/User/UserRepository.swift
@@ -5,7 +5,7 @@ public protocol UserRepository {
     func loadCurrentUserRole() throws -> UserRoleType
     func logout()
     func withdrawal() async throws
-    func addProfileImage(image: Data) async throws
-    func editProfileImage(image: Data) async throws
-    func deleteProfileImage(image: Data) async throws
+    func addProfileImage(profileImage: Data) async throws
+    func deleteProfileImage() async throws
+    func fetchProfileImage() async throws -> String
 }
