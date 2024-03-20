43a329c0bce8d655bde05416c9b23c32e7c28d3a
junseopark0331 <junseopark0331@icloud.com>
2024-03-20 11:30:45 +0900

    🗑️ :: [#220] UserDomain / 사용하지 않는 profileImageEntity 삭제

--- Projects/Domain/UserDomain/Interface/Entity/UserInfo/ProfileImageEntity.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public struct ProfileImageEntity: Equatable {
-    public let profileImage: String?
-
-    public init(profileImage: String?) {
-        self.profileImage = profileImage
-    }
-}
