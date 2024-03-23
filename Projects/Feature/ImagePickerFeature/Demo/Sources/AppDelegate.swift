32a75f1f317ceb7fa543e488b731cfdf1c230715
junseopark0331 <junseopark0331@icloud.com>
2024-03-23 17:27:22 +0900

    ✨ :: [#220] ImagePickerFeature / AppDelegate 추가

---
+++ Projects/Feature/ImagePickerFeature/Demo/Sources/AppDelegate.swift
@@ -0,0 +1,33 @@
+@testable import ImagePickerFeature
+import Inject
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        let store = ImagePickerStore()
+
+        let viewController = Inject.ViewControllerHost(
+            ImagePickerController()
+        )
+
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        supportedInterfaceOrientationsFor window: UIWindow?
+    ) -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
+}
