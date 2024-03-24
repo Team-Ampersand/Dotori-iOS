67de65edbdfda06f37e023f3fc9ed49e746e0cf3
junseopark0331 <junseopark0331@icloud.com>
2024-03-25 08:53:46 +0900

    ♻️ :: [#220] ProfileImageFeature / 이미지 선택 후 버튼 크기를 위해 상수로 고정

--- Projects/Feature/ProfileImageFeature/Sources/Scene/ProfileImageController.swift
+++ Projects/Feature/ProfileImageFeature/Sources/Scene/ProfileImageController.swift
@@ -62,6 +62,8 @@ final class ProfileImageViewController: BaseStoredModalViewController<ProfileIma
                 }
 
                 addImageButton
+                    .width(272)
+                    .height(266)
 
                 confirmButton
             }
