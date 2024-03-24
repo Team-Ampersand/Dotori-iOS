59e817cf780f62250c9fda83f8ea8093bd2fdcb3
junseopark0331 <junseopark0331@icloud.com>
2024-03-24 19:10:53 +0900

    ✨ :: [#220]  ProfileImageFeature / AddImageButtonConfigutionGenerater 추가

---
+++ Projects/Feature/ProfileImageFeature/Sources/Scene/AddImageButtonConfiguration.swift
@@ -0,0 +1,33 @@
+import BaseFeature
+import Combine
+import ConcurrencyUtil
+import DesignSystem
+import Foundation
+import Localization
+import MSGLayout
+import Nuke
+import UIKit
+import UIKitUtil
+import UserDomainInterface
+import YPImagePicker
+
+public enum AddImageButtonConfigutionGenerater {
+    public static func generate() -> UIButton.Configuration {
+        var configuration = UIButton.Configuration.filled()
+        configuration = UIButton.Configuration.filled()
+        configuration.image = UIImage(systemName: "camera.fill")
+        configuration.imagePlacement = .top
+        configuration.imagePadding = 10
+        configuration.background.backgroundColor = .dotori(.background(.bg))
+        configuration.title = L10n.ProfileImage.addImage
+        configuration.titleAlignment = .center
+        configuration.attributedTitle?.foregroundColor = .dotori(.neutral(.n20))
+        configuration.attributedTitle?.font = .dotori(.caption)
+        configuration.baseForegroundColor = .dotori(.primary(.p30))
+        configuration.background.cornerRadius = 8
+        configuration.contentInsets = NSDirectionalEdgeInsets(
+            top: 112, leading: 108, bottom: 104, trailing: 108
+        )
+        return configuration
+    }
+}
