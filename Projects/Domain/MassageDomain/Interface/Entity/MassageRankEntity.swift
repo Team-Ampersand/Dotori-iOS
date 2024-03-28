0e9b49679f603d52ac027034cc3c008508802f05
junseopark0331 <junseopark0331@icloud.com>
2024-03-28 09:07:06 +0900

    ✨ :: [#237] MassageDomain / MassageRankEntity에 profileImage 추가

--- Projects/Domain/MassageDomain/Interface/Entity/MassageRankEntity.swift
+++ Projects/Domain/MassageDomain/Interface/Entity/MassageRankEntity.swift
@@ -7,12 +7,16 @@ public struct MassageRankEntity: Equatable {
     public let stuNum: String
     public let memberName: String
     public let gender: GenderType
+    public let profileImage: String?
 
-    public init(id: Int, rank: Int, stuNum: String, memberName: String, gender: GenderType) {
+    public init(
+        id: Int, rank: Int, stuNum: String, memberName: String, gender: GenderType, profileImage: String
+    ) {
         self.id = id
         self.rank = rank
         self.stuNum = stuNum
         self.memberName = memberName
         self.gender = gender
+        self.profileImage = profileImage
     }
 }
